1db675c0557cbeeeb706ca6ab45be4b67e53103e
Bradford Ware <bware8@gatech.edu>
2016-03-13 19:22:45 -0400

    latest changes

--- js/main.js
+++ js/main.js
@@ -10,39 +10,39 @@ $(document).ready(function() {
   var path = d3.geo.path()
       .projection(projection);
 
-  var svg = d3.select("main").append("svg")
-      .attr("width", width)
-      .attr("height", height);
+  var svg = d3.select('main').append('svg')
+      .attr('width', width)
+      .attr('height', height);
 
-  svg.append("rect")
-      .attr("class", "background")
-      .attr("width", width)
-      .attr("height", height)
-      .on("click", reset);
+  svg.append('rect')
+      .attr('class', 'background')
+      .attr('width', width)
+      .attr('height', height)
+      .on('click', reset);
 
-  var g = svg.append("g")
-      .style("stroke-width", "1.5px");
+  var g = svg.append('g')
+      .style('stroke-width', '1.5px');
 
-  d3.json("/data/us_states.json", function(error, us) {
+  d3.json('/data/us_states.json', function(error, us) {
     if (error) throw error;
 
-    g.selectAll("path")
+    g.selectAll('path')
         .data(topojson.feature(us, us.objects.states).features)
-      .enter().append("path")
-        .attr("d", path)
-        .attr("class", "feature")
-        .on("click", clicked);
+      .enter().append('path')
+        .attr('d', path)
+        .attr('class', 'state')
+        .on('click', clicked);
 
-    g.append("path")
+    g.append('path')
         .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
-        .attr("class", "mesh")
-        .attr("d", path);
+        .attr('class', 'mesh')
+        .attr('d', path);
   });
 
   function clicked(d) {
     if (active.node() === this) return reset();
-    active.classed("active", false);
-    active = d3.select(this).classed("active", true);
+    active.classed('active', false);
+    active = d3.select(this).classed('active', true);
 
     var bounds = path.bounds(d),
         dx = bounds[1][0] - bounds[0][0],
@@ -54,17 +54,17 @@ $(document).ready(function() {
 
     g.transition()
         .duration(750)
-        .style("stroke-width", 1.5 / scale + "px")
-        .attr("transform", "translate(" + translate + ")scale(" + scale + ")");
+        .style('stroke-width', 1.5 / scale + 'px')
+        .attr('transform', 'translate(' + translate + ')scale(' + scale + ')');
   }
 
   function reset() {
-    active.classed("active", false);
+    active.classed('active', false);
     active = d3.select(null);
 
     g.transition()
         .duration(750)
-        .style("stroke-width", "1.5px")
-        .attr("transform", "");
+        .style('stroke-width', '1.5px')
+        .attr('transform', '');
   }
 });
